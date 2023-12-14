a303d3b571221bce16eeee2c8fb948208ff4c145
anticultist <33195947+anticultist@users.noreply.github.com>
2023-12-14 21:08:48 +0100

    add a test for empty string

--- server/test/debug-prints.test.ts
+++ server/test/debug-prints.test.ts
@@ -1,5 +1,19 @@
-import { describe, expect, test } from '@jest/globals';
+import { beforeAll, describe, expect, test } from '@jest/globals';
+
+import * as Parser from 'web-tree-sitter';
+import { loadParser } from '../src/parser';
+import { addDebugPrintsToFunctions } from '../src/debug-prints-adder';
+
+let parser: Parser;
+
+beforeAll(async () => {
+  parser = await loadParser();
+});
 
 describe('add debug prints to functions', () => {
-  test('test empty function', () => {});
+  test('test empty string', async () => {
+    const tree = parser.parse('');
+    const edits = addDebugPrintsToFunctions(tree);
+    expect(edits.length).toEqual(0);
+  });
 });
